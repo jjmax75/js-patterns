de9ab82a3b51b4e7abcbd0727129072dee8f9be9
John Behan <jbehan@mywebgrocer.com>
2019-06-06 12:52:40 +0100

    setup ObserverList

--- observer.js
+++ observer.js
@@ -1,2 +1,27 @@
 // observer pattern
+function ObserverList(){
+  this.observerList = [];
+}
+
+ObserverList.prototype.add = function(obj) {
+  return this.observerList.push(obj);
+};
+
+ObserverList.prototype.count = function() {
+  return this.observerList.length;
+};
+
+ObserverList.prototype.get = function(index) {
+  if (index >= 0 && index < this.count) {
+    return this.observerList[index];
+  }
+};
+
+ObserverList.prototype.indexOf = function(obj, startIndex = 0) {
+  return this.observerList.indexOf(obj, startIndex);
+};
+
+ObserverList.prototype.removeAt = function(index) {
+  this.observerList.splice(index, 1);
+};
 
